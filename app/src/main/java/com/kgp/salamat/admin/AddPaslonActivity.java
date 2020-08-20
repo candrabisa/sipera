e5b5ceb2a793b33e495d087528f7a22b44d823d6
misbachulmunir <misbachulmunir29052gmail.com>
2020-08-20 21:11:58 +0700

    admin

---
+++ app/src/main/java/com/kgp/salamat/admin/AddPaslonActivity.java
@@ -0,0 +1,16 @@
+package com.kgp.salamat.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kgp.salamat.R;
+
+public class AddPaslonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_paslon);
+    }
+}
---
+++ app/src/main/java/com/kgp/salamat/admin/TerimaRelawanActivity.java
@@ -0,0 +1,16 @@
+package com.kgp.salamat.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kgp.salamat.R;
+
+public class TerimaRelawanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terima_relawan);
+    }
+}
